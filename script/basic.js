c0dfc97a396e807108f013cbbab2c3158f3362b2
MuteMini <63823931+MuteMini@users.noreply.github.com>
2020-06-10 02:08:20 -0400

    finished first and second page review

--- script/basic.js
+++ script/basic.js
@@ -1,10 +1,16 @@
-var answerKey = ["abracadabra"];
+var answerKey = ["abracadabra", "1", "1", "1", "1"];
 
 function validateForm(key){
     var x = document.forms["password"]["password"].value;
+    var htmlLink;
     if (x == answerKey[key]) {
         document.getElementById("nofity").innerHTML = "Great Job! - You'll be redirected soon.";
-        setTimeout(() => { window.location.href = "page1.html";}, 2000)
+        if(key < 4){
+            htmlLink = "page"+(key+1)+".html";
+        } else {
+            htmlLink = "prize.html";
+        }
+        setTimeout(() => { window.location.href = htmlLink;}, 2000)
     } else {
         document.getElementById("nofity").innerHTML = "Huh, something is wrong. Double-check your password.";
         return false;
